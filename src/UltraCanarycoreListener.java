5d39f414dcec8f1e9dad21411b427acf559ce969
NiccosSystem <niccossystem@gmail.com>
2013-03-26 03:19:01 +0100

    Trying to do stuff in another way
    Failing to do so, the health change is canceled, but then suddenly
    healed at the end.

--- src/UltraCanarycoreListener.java
+++ src/UltraCanarycoreListener.java
@@ -1,10 +1,17 @@
 
 public class UltraCanarycoreListener extends PluginListener{
 
-	public int onFoodLevelChange(Player player, int oldFoodLevel, int newFoodLevel) {
-		return newFoodLevel >= 18 ? 17 : newFoodLevel;		
+	public boolean onHealthChange(Player p, int oldValue, int newValue) {
+		return newValue > oldValue ? true : false;
 	}
 	public boolean onCommand(Player p, String[] cmd) {		
 		return false;		
+	}
+	public boolean onEat(Player p, Item item) {
+		if (item.getItemId() == 322) {
+			p.setHealth(p.getHealth() + 4);
+			return false;
+		}
+		return false;
 	}
 }
