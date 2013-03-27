585972c11b7649702ef8d4f2fdb3de189a11c82f
NiccosSystem <niccossystem@gmail.com>
2013-03-27 23:29:21 +0100

    Forgot dis

--- src/UltraCanarycoreListener.java
+++ src/UltraCanarycoreListener.java
@@ -3,7 +3,5 @@ public class UltraCanarycoreListener extends PluginListener{
 
 	public boolean onHealthChange(Player p, int oldValue, int newValue) {
 		return newValue > oldValue && oldValue > 0 && !p.isDead() ? true : false;
-		
-	
 	}
 }
